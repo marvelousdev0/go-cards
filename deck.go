5ba0a20f354b9758d6297cf379ff7cf2fa840911
Marvelous Dev <marvelousdev0@gmail.com>
2022-01-25 16:56:14 -0500

    Init cards demo proj

---
+++ deck.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+type deck []string
+
+func (d deck) print() {
+	fmt.Println(d)
+}
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
+func (d deck) deal(handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join(deck(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, error := ioutil.ReadFile(filename)
+	if error != nil {
+		fmt.Println("Error:", error)
+		os.Exit(1)
+	}
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	/* cards := newDeck()
+	hand, remaining := cards.deal(5)
+	hand.print()
+	remaining.print()
+
+	cardString := cards.toString()
+	fmt.Println(cardString)
+
+	cards.saveToFile("my_cards") */
+	cards := newDeckFromFile("my_cards")
+	cards.print()
+}
